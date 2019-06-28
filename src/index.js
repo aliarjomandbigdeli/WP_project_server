17a265dd5561767d1487f7169085ff8b3cec0f8b
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-28 15:06:35 +0430

    run server on port 3000

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log("hello world")
-console.log(`Date : ${Date()}`)
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send('Web Programming Course server')
+});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
+
+// console.log("hello world")
+// console.log(`Date : ${Date()}`)
