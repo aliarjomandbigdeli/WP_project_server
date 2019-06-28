0c7789e2691200eaf67271e8a844cfa69777389c
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-28 20:54:25 +0430

    fixed a warning

--- src/models/restaurant.model.js
+++ src/models/restaurant.model.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 const server = '127.0.0.1:27017';
 const database = 'testDB';
 
-mongoose.connect(`mongodb://${server}/${database}`)
+mongoose.connect(`mongodb://${server}/${database}`,{useNewUrlParser: true})
     .then(() => {
         console.log('Database connection successful')
     })
